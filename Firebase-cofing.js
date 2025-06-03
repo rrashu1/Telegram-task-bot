e6aa5d1c02bcccbf6932e8303e044296b075265a
rrashu1 <joksadda5@gmail.com>
2025-06-03 16:12:43 +0600

    Create Firebase-cofing.js

---
+++ Firebase-cofing.js
@@ -0,0 +1,14 @@
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = { db, auth };
